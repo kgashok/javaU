a4f17ac6e7231862e479eda8d04f7b2494d918b3
ashok baktha <ashok@server>
2014-09-21 02:59:51 +0530

    Added a new JUnit4 - need to fix this.

---
+++ firstJ/firstJ/IntroTest4.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package firstJ;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Advik
+ *
+ */
+public class IntroTest4 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
