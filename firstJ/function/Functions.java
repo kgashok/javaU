07f0c6598e97f69cdfa174481888495db28fb872
ashok baktha <ashok@server>
2014-09-21 13:24:13 +0530

    Remove auto-stub comments.

--- firstJ/function/Functions.java
+++ firstJ/function/Functions.java
@@ -10,37 +10,30 @@ package function;
 public class Functions {
 
 	public static String hello() {
-		// TODO Auto-generated method stub
 		return "hello";
 	}
 
 	public static int add(int i, int j) {
-		// TODO Auto-generated method stub
 		return i+j;
 	}
 
 	public static int ftoc(int i) {
-		// TODO Auto-generated method stub
 		// [°C] = ([°F] - 32) × 5/9
-
 		return (i - 32) * 5 / 9;
 	}
 
 	public static String[] makeArray(String string, String string2,
 			String string3) {
-		// TODO Auto-generated method stub
 		String[] array = new String[3];
 		array[0] = string; array[1] = string2; array[2] = string3;
 		return array;
 	}
 
 	public static int add(int[] numbers) {
-		// TODO Auto-generated method stub
 		int sum = 0;
 		for (int i = 0; i < numbers.length; i++)
 			sum += numbers[i];
 		return sum;
 	}
-
 
 }
